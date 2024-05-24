278799097454853496adb2d73a6ae10dea94a22c
Francesco10998 <64406264+Francesco10998@users.noreply.github.com>
2024-05-24 12:00:04 +0200

    Update gradio_canny2image.py

--- gradio_canny2image.py
+++ gradio_canny2image.py
@@ -24,13 +24,21 @@ model = model.cuda()
 ddim_sampler = DDIMSampler(model)
 
 
-def process(input_image, prompt, a_prompt, n_prompt, num_samples, image_resolution, ddim_steps, guess_mode, strength, scale, seed, eta, low_threshold, high_threshold):
+def process(input_image, counterfactual_image, prompt, a_prompt, n_prompt, num_samples, image_resolution, ddim_steps, guess_mode, strength, scale, seed, eta, low_threshold, high_threshold):
     with torch.no_grad():
         img = resize_image(HWC3(input_image), image_resolution)
         H, W, C = img.shape
 
-        detected_map = apply_canny(img, low_threshold, high_threshold)
-        detected_map = HWC3(detected_map)
+        detected_map_original = apply_canny(img, low_threshold, high_threshold)
+        detected_map_origianl = HWC3(detected_map)
+
+        ########### compute canny edge also of the counterfactual explanation ##############
+        detected_map_counterfactual = apply_canny(counterfactual_img, low_threshold, high_threshold)
+        detected_map_counterfactual = HWC3(detected_map)
+
+        ########### obtain the canny edge of the attribute only ##############
+        detected_map = detected_map_counterfactual - detected_map_original
+        
 
         control = torch.from_numpy(detected_map.copy()).float().cuda() / 255.0
         control = torch.stack([control for _ in range(num_samples)], dim=0)
@@ -91,8 +99,8 @@ with block:
                                       value='longbody, lowres, bad anatomy, bad hands, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality')
         with gr.Column():
             result_gallery = gr.Gallery(label='Output', show_label=False, elem_id="gallery").style(grid=2, height='auto')
-    ips = [input_image, prompt, a_prompt, n_prompt, num_samples, image_resolution, ddim_steps, guess_mode, strength, scale, seed, eta, low_threshold, high_threshold]
+    ips = [input_image, counterfactual_image, prompt, a_prompt, n_prompt, num_samples, image_resolution, ddim_steps, guess_mode, strength, scale, seed, eta, low_threshold, high_threshold]
     run_button.click(fn=process, inputs=ips, outputs=[result_gallery])
 
 
-block.launch(server_name='0.0.0.0')
+block.launch(server_name='0.0.0.0', share=True)
