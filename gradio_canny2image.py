9d2109ae4fd5674292db230b00a469c307c37915
Francesco10998 <64406264+Francesco10998@users.noreply.github.com>
2024-05-30 13:51:06 +0200

    Update gradio_canny2image.py

--- gradio_canny2image.py
+++ gradio_canny2image.py
@@ -16,6 +16,156 @@ from cldm.model import create_model, load_state_dict
 from cldm.ddim_hacked import DDIMSampler
 
 
+######################### STABLE DIFFUSION v1.5 DDIM INVERSION CODE ######################à
+import requests
+import torch.nn as nn
+import torch.nn.functional as F
+from PIL import Image
+from io import BytesIO
+from tqdm.auto import tqdm
+from matplotlib import pyplot as plt
+from torchvision import transforms as tfms
+from diffusers import StableDiffusionPipeline, DDIMScheduler
+
+device =  torch.device("mps" if torch.backends.mps.is_available() else "cuda" if torch.cuda.is_available() else "CPU")
+# Load a pipeline
+pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5").to(device)
+# Set up a DDIM scheduler
+pipe.scheduler = DDIMScheduler.from_config(pipe.scheduler.config)
+
+# Sample function (regular DDIM)
+@torch.no_grad()
+def sample(prompt, start_step=0, start_latents=None,
+           guidance_scale=3.5, num_inference_steps=30,
+           num_images_per_prompt=1, do_classifier_free_guidance=True,
+           negative_prompt='', device=device):
+
+    # Encode prompt
+    text_embeddings = pipe._encode_prompt(
+            prompt, device, num_images_per_prompt, do_classifier_free_guidance, negative_prompt
+    )
+
+    # Set num inference steps
+    pipe.scheduler.set_timesteps(num_inference_steps, device=device)
+
+    # Create a random starting point if we don't have one already
+    if start_latents is None:
+        start_latents = torch.randn(1, 4, 64, 64, device=device)
+        start_latents *= pipe.scheduler.init_noise_sigma
+
+    latents = start_latents.clone()
+
+    for i in tqdm(range(start_step, num_inference_steps)):
+
+        t = pipe.scheduler.timesteps[i]
+
+        # Expand the latents if we are doing classifier free guidance
+        latent_model_input = torch.cat([latents] * 2) if do_classifier_free_guidance else latents
+        latent_model_input = pipe.scheduler.scale_model_input(latent_model_input, t)
+
+        # Predict the noise residual
+        noise_pred = pipe.unet(latent_model_input, t, encoder_hidden_states=text_embeddings).sample
+
+        # Perform guidance
+        if do_classifier_free_guidance:
+            noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
+            noise_pred = noise_pred_uncond + guidance_scale * (noise_pred_text - noise_pred_uncond)
+
+
+        # Normally we'd rely on the scheduler to handle the update step:
+        # latents = pipe.scheduler.step(noise_pred, t, latents).prev_sample
+
+        # Instead, let's do it ourselves:
+        prev_t = max(1, t.item() - (1000//num_inference_steps)) # t-1
+        alpha_t = pipe.scheduler.alphas_cumprod[t.item()]
+        alpha_t_prev = pipe.scheduler.alphas_cumprod[prev_t]
+        predicted_x0 = (latents - (1-alpha_t).sqrt()*noise_pred) / alpha_t.sqrt()
+        direction_pointing_to_xt = (1-alpha_t_prev).sqrt()*noise_pred
+        latents = alpha_t_prev.sqrt()*predicted_x0 + direction_pointing_to_xt
+
+    # Post-processing
+    images = pipe.decode_latents(latents)
+    images = pipe.numpy_to_pil(images)
+
+    return images
+
+
+#input_image = load_image('https://images.pexels.com/photos/8306128/pexels-photo-8306128.jpeg', size=(512, 512))
+#input_image_prompt = "Photograph of a puppy on the grass"
+# Encode with VAE
+with torch.no_grad(): latent = pipe.vae.encode(tfms.functional.to_tensor(input_image).unsqueeze(0).to(device)*2-1)
+l = 0.18215 * latent.latent_dist.sample()
+
+## Inversion
+@torch.no_grad()
+def invert(start_latents, prompt, guidance_scale=3.5, num_inference_steps=80,
+           num_images_per_prompt=1, do_classifier_free_guidance=True,
+           negative_prompt='', device=device):
+
+    # Encode prompt
+    text_embeddings = pipe._encode_prompt(
+            prompt, device, num_images_per_prompt, do_classifier_free_guidance, negative_prompt
+    )
+
+    # Latents are now the specified start latents
+    latents = start_latents.clone()
+
+    # We'll keep a list of the inverted latents as the process goes on
+    intermediate_latents = []
+
+    # Set num inference steps
+    pipe.scheduler.set_timesteps(num_inference_steps, device=device)
+
+    # Reversed timesteps <<<<<<<<<<<<<<<<<<<<
+    timesteps = reversed(pipe.scheduler.timesteps)
+
+    for i in tqdm(range(1, num_inference_steps), total=num_inference_steps-1):
+
+        # We'll skip the final iteration
+        if i >= num_inference_steps - 1: continue
+
+        t = timesteps[i]
+
+        # Expand the latents if we are doing classifier free guidance
+        latent_model_input = torch.cat([latents] * 2) if do_classifier_free_guidance else latents
+        latent_model_input = pipe.scheduler.scale_model_input(latent_model_input, t)
+
+        # Predict the noise residual
+        noise_pred = pipe.unet(latent_model_input, t, encoder_hidden_states=text_embeddings).sample
+
+        # Perform guidance
+        if do_classifier_free_guidance:
+            noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
+            noise_pred = noise_pred_uncond + guidance_scale * (noise_pred_text - noise_pred_uncond)
+
+        current_t = max(0, t.item() - (1000//num_inference_steps)) #t
+        next_t = t # min(999, t.item() + (1000//num_inference_steps)) # t+1
+        alpha_t = pipe.scheduler.alphas_cumprod[current_t]
+        alpha_t_next = pipe.scheduler.alphas_cumprod[next_t]
+
+        # Inverted update step (re-arranging the update step to get x(t) (new latents) as a function of x(t-1) (current latents)
+        latents = (latents - (1-alpha_t).sqrt()*noise_pred)*(alpha_t_next.sqrt()/alpha_t.sqrt()) + (1-alpha_t_next).sqrt()*noise_pred
+
+
+        # Store
+        intermediate_latents.append(latents)
+
+    return torch.cat(intermediate_latents)
+
+inverted_latents = invert(l, input_image_prompt, num_inference_steps=50)
+#inverted_latents.shape
+# Decode the final inverted latents
+"""
+with torch.no_grad():
+  im = pipe.decode_latents(inverted_latents[-1].unsqueeze(0))
+pipe.numpy_to_pil(im)[0] 
+
+# The reason we want to be able to specify start step
+start_step = 20
+sample(input_image_prompt, start_latents=inverted_latents[-(start_step+1)][None],
+start_step=start_step, num_inference_steps=50)[0]
+"""
+#####################################################################àà
 apply_canny = CannyDetector()
 
 model = create_model('./models/cldm_v15.yaml').cpu()
@@ -34,9 +184,6 @@ def process(input_image, counterfactual_image, prompt, a_prompt, n_prompt, num_s
         detected_map_original = apply_canny(img, low_threshold, high_threshold)
         detected_map_original = HWC3(detected_map_original)
         """
-        ########### encode the original image ################
-        
-        
 
         ########### compute canny edge also of the counterfactual explanation ##############
         c_img = resize_image(HWC3(counterfactual_image), image_resolution)
@@ -45,13 +192,6 @@ def process(input_image, counterfactual_image, prompt, a_prompt, n_prompt, num_s
 
         ########### obtain the canny edge of the attribute only ##############
         #detected_map = cv2.absdiff(detected_map_counterfactual, detected_map_original)
-
-        """
-        ########## Optional: pulizia dell'immagine dei bordi degli occhiali ##########
-        kernel = np.ones((3, 3), np.uint8)
-        detected_map = cv2.morphologyEx(detected_map, cv2.MORPH_CLOSE, kernel)
-        detected_map = cv2.morphologyEx(detected_map, cv2.MORPH_OPEN, kernel)
-        """
 
         control = torch.from_numpy(detected_map_counterfactual.copy()).float().cuda() / 255.0
         control = torch.stack([control for _ in range(num_samples)], dim=0)
@@ -72,17 +212,24 @@ def process(input_image, counterfactual_image, prompt, a_prompt, n_prompt, num_s
             model.low_vram_shift(is_diffusing=True)
 
         model.control_scales = [strength * (0.825 ** float(12 - i)) for i in range(13)] if guess_mode else ([strength] * 13)  # Magic number. IDK why. Perhaps because 0.825**12<0.01 but 0.826**12>0.01
+
+        # encoding of the image using ddim inversion
+        # Encode with VAE
+        with torch.no_grad(): latent = pipe.vae.encode(tfms.functional.to_tensor(input_image).unsqueeze(0).to(device)*2-1)
+        l = 0.18215 * latent.latent_dist.sample()
+
+        inverted_latents = invert(l, img, num_inference_steps=50)
         
-        input_image_64 = resize_image(HWC3(input_image), 64)
+        """input_image_64 = resize_image(HWC3(input_image), 64)
         ten = torch.Tensor(input_image_64).permute(2,1,0).cuda()
         alpha_channel = torch.ones((1, ten.shape[1], ten.shape[2])).cuda()
 
         # Concatenare il canale alpha all'immagine RGB
         rgba_tensor = torch.cat((ten, alpha_channel), dim=0)
-        x_next, noise = ddim_sampler.encode(rgba_tensor.cuda(), cond, t_enc=20)
+        x_next, noise = ddim_sampler.encode(rgba_tensor.cuda(), cond, t_enc=20)"""
 
         samples, intermediates = ddim_sampler.sample(ddim_steps, num_samples,
-                                                     shape, cond, verbose=False, eta=eta, x_T=noise,
+                                                     shape, cond, verbose=False, eta=eta, x_T=inverted_latents,
                                                      unconditional_guidance_scale=scale,
                                                      unconditional_conditioning=un_cond)
         """samples, intermediates = ddim_sampler.sample(ddim_steps, num_samples,
